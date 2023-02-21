518a9c4576a99d376bc8882361655cbcf2297f3c
Olivier-Kango <olivierkango@gmail.com>
2023-02-21 06:23:07 +0200

    Add devise

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# ApplicationRecord
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
