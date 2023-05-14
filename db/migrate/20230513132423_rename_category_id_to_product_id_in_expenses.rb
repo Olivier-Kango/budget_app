9270b0344c96eea28463b28164971ef2102e820a
Olivier-Kango <olivierkango@gmail.com>
2023-05-14 17:02:54 +0300

    migration g change products to categories

--- db/migrate/20230513132423_rename_category_id_to_product_id_in_expenses.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class RenameCategoryIdToProductIdInExpenses < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :expenses, :category_id, :product_id
-  end
-end
