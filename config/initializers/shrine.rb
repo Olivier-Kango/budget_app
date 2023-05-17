ddc87650ef2fda293c2e90c8b10b89d0590266f1
Olivier-Kango <olivierkango@gmail.com>
2023-05-17 03:10:22 +0300

    Remove extra space

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -16,6 +16,6 @@ Shrine.storages = if Rails.env.test?
                   end
 Shrine.plugin :activerecord # loads Active Record integration
 Shrine.plugin :cached_attachment_data # enables retaining cached file across form redisplays
-Shrine.plugin :restore_cached_data  # extracts metadata for assigned cached files
+Shrine.plugin :restore_cached_data # extracts metadata for assigned cached files
 Shrine.plugin :validation_helpers
 Shrine.plugin :validation
