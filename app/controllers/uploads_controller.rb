5be0931eced693176d90314315246b5f893cc8b9
Olivier-Kango <olivierkango@gmail.com>
2023-05-17 17:05:08 +0300

    Fix linter errors

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -4,13 +4,13 @@ class UploadsController < ApplicationController
   def show
     # Retrieve the filename from the URL parameter
     filename = params[:filename]
-    
+
     # Define the directory where the uploads are stored
     uploads_directory = Rails.root.join('public', 'uploads')
-    
+
     # Determine the file path
     file_path = File.join(uploads_directory, filename)
-    
+
     if File.exist?(file_path)
       # If the file exists, send it as a response
       send_file file_path, disposition: 'inline'
--- db/migrate/20230517085056_add_category_id_to_expenses.rb
+++ db/migrate/20230517085056_add_category_id_to_expenses.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddCategoryIdToExpenses < ActiveRecord::Migration[7.0]
   def change
     add_column :expenses, :category_id, :bigint unless column_exists?(:expenses, :category_id)
